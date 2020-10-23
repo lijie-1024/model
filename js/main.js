3a48624b1d3836e1eb4620beedc31760440d8173
lijie <992276586@qq.com>
2020-10-23 17:22:40 +0800

    1-0

--- js/main.js
+++ js/main.js
@@ -1,36 +1,18 @@
-// ;(function(doc, win) {
-//   var docEl = doc.documentElement,
-//     resizeEvt = 'orientationchange' in window ? 'orientationchange' : 'resize',
-//     recalc = function() {
-//       var clientWidth = docEl.clientWidth
-//       if (!clientWidth) return
-//       if (clientWidth >= 750) {
-//         docEl.style.fontSize = '16px'
-//       } else {
-//         docEl.style.fontSize = 16 * (clientWidth / 750) + 'px'
-//       }
-//     }
-//   if (!doc.addEventListener) return
-//   win.addEventListener(resizeEvt, recalc, false)
-//   doc.addEventListener('DOMContentLoaded', recalc, false)
-// })(document, window)
-
-
-
 
 ;(function () {
   // 屏幕大于800
   var designW = 750
   var font_rate = 16
+  var browserWidth = window.innerWidth || document.documentElement.clientWidth || document.body.clientWidth;
   //适配
-  document.getElementsByTagName('html')[0].style.fontSize = (document.body.offsetWidth / designW) * font_rate + 'px'
-  document.getElementsByTagName('body')[0].style.fontSize = (document.body.offsetWidth / designW) * font_rate + 'px'
+  document.getElementsByTagName('html')[0].style.fontSize = (browserWidth / designW) * font_rate + 'px'
+  document.getElementsByTagName('body')[0].style.fontSize = (browserWidth / designW) * font_rate + 'px'
   //监测窗口大小变化
   window.addEventListener(
     'onorientationchange' in window ? 'orientationchange' : 'resize',
     function () {
-      document.getElementsByTagName('html')[0].style.fontSize = (document.body.offsetWidth / designW) * font_rate + 'px'
-      document.getElementsByTagName('body')[0].style.fontSize = (document.body.offsetWidth / designW) * font_rate + 'px'
+      document.getElementsByTagName('html')[0].style.fontSize = (browserWidth / designW) * font_rate + 'px'
+      document.getElementsByTagName('body')[0].style.fontSize = (browserWidth / designW) * font_rate + 'px'
     },
     false
   )
